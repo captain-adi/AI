555888e06f0f7a7a2d4f7890427fb0f7d0d58aa4
captain-adi <maniramp554@gmail.com>
2025-09-08 16:05:53 +0530

    first commit

---
+++ datatype/01.py
@@ -0,0 +1,16 @@
+#intiger
+a = 10
+
+
+isboiling = True
+
+
+#string
+name = "John"
+greeting = 'Hello, World!'
+
+print(f"the first word of greeting is : {greeting[:5]}")
+print(f"the last word of greeting is : {greeting[7:]}")
+
+lable = "this is  string"
+
