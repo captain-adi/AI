a72df8dcb03d1ecd197484bdf071c51b6ce50430
captain-adi <maniramp554@gmail.com>
2025-09-10 19:46:32 +0530

    conditional statement

---
+++ conditional/conditional.py
@@ -0,0 +1,12 @@
+a = 200 
+b = 2001
+if a>b:
+    print("a is greater than b")
+elif b>a:
+    print("b is grater than a ")
+else:
+    print("a and b is equal")
+
+
+
+print("a is greater than b") if a>b else print("b")
