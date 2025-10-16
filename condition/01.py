8be6986f3dd43e237fca884ddbf76ba6567fddcd
captain-adi <maniramp554@gmail.com>
2025-10-16 11:05:31 +0530

    feat: practice basic conditional statements examples

---
+++ condition/01.py
@@ -0,0 +1,62 @@
+# num = int(input("Enter a Number : "))
+# if num%2==0:
+#     print("Even")
+# else:
+#     print("odd")
+
+
+
+# 2 .  Positive, Negative, or Zero
+# num = int(input("Enter a Number : "))
+# if num > 0 : 
+#     print("positive")
+# elif num < 0 :
+#     print("negetive")
+# else: 
+#     print("Zero")
+
+
+
+# 03.  Largest of Two Numbers
+
+# num1 = 1223
+# num2 = 15
+# if num1 > num2:
+#     print(f"{num1} is biggest ")
+# else:
+#     print(f"{num2} is greater")
+
+
+
+
+# 04. Vowel or Consonant
+# ch = "s"
+# vowels = ["a" , "e" , "i" , "o" , "u"]
+# if ch in  vowels:
+#     print("the Charecter is Vowel")
+# else:
+#     print("The Charecter is Consonant")
+
+ 
+
+# 05 . Age Eligibility for Voting
+# age = int(input("Enter You Age : "))
+# if age >= 18:
+#     print("you can vote")
+# else:
+#     print("you can not Vote")
+
+
+
+# 06. Odd, Even, and Multiple of 5
+# num = int(input("Enter a Number : "))
+# if num%2==0 and num%5==0:
+#     print("Even and multiple of 5")
+# elif num%2==0:
+#     print("Even Only")
+# else :
+#     print("odd Only")
+
+
+# num = int(input("Enter a Number : "))
+# print("Even" if num%2==0 else "odd")
